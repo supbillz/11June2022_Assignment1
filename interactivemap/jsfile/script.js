3a916ec32e1665200bea8dc388c71163f30690aa
supbillz <hoferbillysolomon@gmail.com>
2022-06-21 11:29:52 +0000

    Initial Commit

--- interactivemap/jsfile/script.js
+++ interactivemap/jsfile/script.js
@@ -1,4 +1,4 @@
-const BASE_API_URL = "https://api.foursquare.com/v3";
+/*const BASE_API_URL = "https://api.foursquare.com/v3";
 const API_KEY = "fsq3d8HsaUFx6m1QmbJGDLHwSmEK4DMnknQuUWYfjP/8CdI=";
 const headers = {
     "Accept": 'application/json',
@@ -7,7 +7,7 @@ const headers = {
 
 
 
-/*async function main() {
+async function main() {
     let map = boilerMap();
 
     let response = await axios.get('qcentres.geojson');
@@ -20,9 +20,9 @@ const headers = {
 }
 main()*/
 
-async function appearJson(){
+async function appearQuitGeoJson(){
     let map = boilerMap();
-let response = await axios.get('qcentres.geojson');
+let response = await axios.get('geojson/quitcentres.geojson');
 let qcentreLayer = L.geoJson(response.data, {
     onEachFeature: function(feature, layer){
         console.log(feature);
@@ -31,4 +31,19 @@ let qcentreLayer = L.geoJson(response.data, {
 })
 qcentreLayer.addTo(map);
 }
-appearJson();
+
+async function appearPharmaGeoJson(){
+    let map = boilerMap();
+let pharmaResponse = await axios.get('geojson/pharmastores.geojson');
+let pharmaLayer = L.geoJson(pharmaResponse.data, {
+    onEachFeature: function(feature, layer){
+        console.log(feature);
+        layer.bindPopup(feature.properties.Description);
+    }
+})
+pharmaLayer.addTo(map);
+}
+
+appearQuitGeoJson();
+appearPharmaGeoJson()
+
